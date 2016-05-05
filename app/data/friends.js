02ec4a838d50c5582f19e99530021d527ce185a1
ceznik <ceznik@verizon.net>
2016-05-05 18:06:42 -0400

    initial commit: folder structure, npm package setup, bootstrap

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+var friendsList = [
+    {
+        'name': 'Shawn Singh',
+        'photo': '//photo here',
+        'scores': [1,2,3,4,5,4,3,2,1,2]
+    }
+];
+
+module.exports = friendsList;
---
+++ app/routing/api-routes.js
@@ -0,0 +1,12 @@
+var friendsList = require('../data/friends.js');
+var path = require('path');
+
+module.exports = function (app) {
+    app.get('/api/friends', function (req, res) {
+        res.json(friendsList);
+    });
+
+    app.post('/api/friends', function (req, res) {
+        console.log("friend logic happens here.");
+    })
+}
